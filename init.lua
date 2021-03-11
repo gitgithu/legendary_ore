d44e22dc2f420eda2c4945964fbbccaa4f66a6ec
DynamaxPikachu <46947146+gitgithu@users.noreply.github.com>
2021-03-11 17:09:16 -0500

    Update init.lua
    legendary_armor support, fixing mapgen

--- init.lua
+++ init.lua
@@ -28,14 +28,25 @@ minetest.register_craft({
     }
 })
 
-minetest.register_node("legendary_ore:legendary_ore", {
-	description = S("Legendary ore"),
-	tiles = {"default_stone.png^legendary_ore_ore.png"},
-	groups = {cracky = 3},
-	sounds = default_stone_sounds,
-        light_source = 7,
-	drop = "legendary_armor:ingot",
-})
+if minetest.get_modpath("legendary_armor") then
+	minetest.register_node("legendary_ore:legendary_ore", {
+		description = S("Legendary ore"),
+		tiles = {"default_stone.png^legendary_ore_ore.png"},
+		groups = {cracky = 3},
+		sounds = default_stone_sounds,
+			light_source = 7,
+		drop = "legendary_armor:ingot",
+	})
+else
+	minetest.register_node("legendary_ore:legendary_ore", {
+		description = S("Legendary ore"),
+		tiles = {"default_stone.png^legendary_ore_ore.png"},
+		groups = {cracky = 3},
+		sounds = default_stone_sounds,
+			light_source = 7,
+		drop = "legendary_ore:legendary_ore",
+	})
+end
 
 
 -- Mapgen 
@@ -46,8 +57,8 @@ minetest.register_node("legendary_ore:legendary_ore", {
 	          ore            = "legendary_ore:legendary_ore",
 	          wherein        = "default:stone",
 	          clust_scarcity = 65 * 65 * 65,
-	          clust_num_ores = 5,
-	          clust_size     = 5,
-	          y_max          = -140,
+	          clust_num_ores = 2,
+	          clust_size     = 2,
+	          y_max          = -450,
 	          y_min          = -31000,
 	   })
