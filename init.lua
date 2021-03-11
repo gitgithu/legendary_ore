d56b3042613f694111d561a464c03631dc72183d
DynamaxPikachu <46947146+gitgithu@users.noreply.github.com>
2021-03-11 16:51:30 -0500

    Update init.lua
    adding more_mese support, fixing groups

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ minetest.register_node("legendary_ore:legendary_block", {
     tiles = {"legendary_ore_block.png"},
     light_source = 7,
     is_ground_content = true,
-    groups = {cracky=3, stone=1}
+    groups = {cracky=6}
 })
 
 minetest.register_craft({
@@ -31,10 +31,13 @@ minetest.register_craft({
 minetest.register_node("legendary_ore:legendary_ore", {
 	description = S("Legendary ore"),
 	tiles = {"default_stone.png^legendary_ore_ore.png"},
-	groups = {cracky = 2},
+	groups = {cracky = 6},
 	sounds = default_stone_sounds,
         light_source = 7,
-	drop = "legendary_armor:ingot",
+	if minetest.get_modpath("legendary_armor") then
+		drop = "legendary_armor:ingot",
+	else
+		drop = "legendary_ore:legendary_ore"
 })
 
 
@@ -46,8 +49,8 @@ minetest.register_node("legendary_ore:legendary_ore", {
 	          ore            = "legendary_ore:legendary_ore",
 	          wherein        = "default:stone",
 	          clust_scarcity = 65 * 65 * 65,
-	          clust_num_ores = 2,
-	          clust_size     = 2,
-	          y_max          = -450,
+	          clust_num_ores = 5,
+	          clust_size     = 5,
+	          y_max          = -140,
 	          y_min          = -31000,
 	   })
